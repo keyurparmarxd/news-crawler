328bfcca06c499c898cb819b61d7f78a62cd6092
Keyur Parmar <92371740+keyurparmarxd@users.noreply.github.com>
2024-04-16 13:05:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ image = Image.open("logo.png")
 st.image(image)
 
 # Web App Title
-st.title('''**News Crawler**''')
+st.title('''**IntelliAdora's News Crawler**''')
 
 #Fixed format of crawling site link
 url = 'https://news.google.com/rss/search?q='
@@ -78,31 +78,20 @@ if input_operation == "Search News":
             links = pd.DataFrame(links['Links'])
             top_links = links.head(50)
 
-            def extract_domain(link):
-    # Extract the domain name from the link
-    domain = re.findall(r'(?<=https?://)[^/]+', link)
-    if domain:
-        return domain[0]
-    else:
-        return "Read More"
+            def make_clickable(link):
+                # target _blank to open new window
+                # extract clickable text to display for your link
+                #text = link.split('=')[1]
+                return f'<a target="_blank" href="{link}">{link}</a>'
 
 
-def make_clickable(link):
-    # Create a clickable link
-    return f'<a target="_blank" href="{link}">Read More</a>'
+            # link is the column with hyperlinks
+            links['Links'] = links['Links'].apply(make_clickable)
 
 
-# Apply the functions to the links
-links['Links'] = links['Links'].apply(make_clickable)
-links['Links'] = links['Links'].apply(extract_domain)
-
-# Combine the dataframes
-OUTPUT = pd.concat([headlines, links, source], axis=1)  # Normal
-
-# Display the modified dataframe
-st.info("Articles. as per ranking")
-st.write(OUTPUT.to_html(escape=False, index=False), unsafe_allow_html=True)
-
+            OUTPUT = pd.concat([headlines, links, source], axis=1)  # Normal
+            st.info("Articles. as per ranking")
+            st.write(OUTPUT.to_html(escape=False, index=False), unsafe_allow_html=True)
 
 
         elif input_sub_operation == "Display news summary":
@@ -271,7 +260,6 @@ st.write(OUTPUT.to_html(escape=False, index=False), unsafe_allow_html=True)
 
                 SENTIMENT_OUTPUT = SENTIMENT_OUTPUT.drop(["Article","Sentiment"], axis =1)
                 st.write(SENTIMENT_OUTPUT.head(50).to_html(escape=False, index=False), unsafe_allow_html=True)
-
 
 
 
